3b282a1838bd6870ddb114aa41b0bf267f19c837
eMIKARp <emil.karpowicz@gmail.com>
2018-07-11 17:17:36 +0200

    Homepage category sidebar modified

--- src/main/java/pl/myrecipebasket/web/controller/HomeController.java
+++ src/main/java/pl/myrecipebasket/web/controller/HomeController.java
@@ -1,5 +1,6 @@
 package pl.myrecipebasket.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,27 @@ import pl.myrecipebasket.model.Recipe;
 import pl.myrecipebasket.model.Role;
 import pl.myrecipebasket.model.User;
 import pl.myrecipebasket.model.Vote;
+import pl.myrecipebasket.service.CategoryService;
 
 @Controller
 public class HomeController {
 
+	CategoryService categoryService;
+	
+	@Autowired
+	public void setCategoryService(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+	
 	@GetMapping("/")
 	public String home(Model model) {
+		model.addAttribute("categories", categoryService.getCategories());
 		return "homepage";
 	}
 	
 	@GetMapping("/homepage")
 	public String homepage(Model model) {
+		model.addAttribute("categories", categoryService.getCategories());
 		return "homepage";
 	}
 	
